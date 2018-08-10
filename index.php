8eb60d64bf9ae1ccfd6df6679e5ceb4e34c6c1f9
yuki <yuki@test.com>
2018-08-10 11:54:21 +0000

    countdown

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Today is 8/8, 7 business Days until X-DAY !!</p>'; ?>
+ <?php echo '<p>Today is 8/10, 5 business Days until X-DAY !!</p>'; ?>
  </body>
 </html>
