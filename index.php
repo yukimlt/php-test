4378042f8840d6308fe655fd62d059ba354e89d0
kana <kana@test.com>
2018-08-14 01:28:09 +0000

    countdowngit config --global user.name kana

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Today is 8/10, 5 business Days until X-DAY !!</p>'; ?>
+ <?php echo '<p>Today is 8/14, 3 business Days until X-DAY !!</p>'; ?>
  </body>
 </html>
